f585d988c1781d486e30c34bab330873ce66235d
turjo46 <turjoemon416@gmail.com>
2025-02-22 16:02:20 +0600

    second update

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,12 +1,93 @@
-import React from 'react'
-import "./main.css"
+import React, { useContext } from 'react';
+import "./main.css";
+import { assets } from '../../assets/assets';
+import { Context } from '../../context/Context';
 
 const Main = () => {
+
+
+  const {onSent,recentPrompt,showResult,loading,resultData,setInput,input} = useContext(Context)
+
+
+
   return (
-    <div>
+    <div className='main'>
+      <div className="nav">
+        <p>Gemini</p>
+        <img src={assets.user_icon} alt="User profile icon" />
+      </div>
+      <div className="main-container">
 
+        {!showResult?
+        <>
+        <div className="greet">
+          <p><span>Hello, dev.</span></p>
+          <p>How can I help you today?</p>
+        </div>
+        <div className="cards">
+          <div className="card">
+            <p>Suggest beautiful places to see on an upcoming road trip</p>
+            <img src={assets.compass_icon} alt="Compass icon" />
+          </div>
+          <div className="card">
+            <p>Briefly summarize this concept: Urban Planning</p>
+            <img src={assets.bulb_icon} alt="Light bulb icon" />
+          </div>
+          <div className="card">
+            <p>Brainstorm team bonding activities for our retreat</p>
+            <img src={assets.message_icon} alt="Message icon" />
+          </div>
+          <div className="card">
+            <p>Improve the readability of the following code</p>
+            <img src={assets.code_icon} alt="Code icon" />
+          </div>
+        </div>
+        
+
+        
+
+        </>
+        : <div className="result">
+          <div className="result-title">
+            <img src={assets.user_icon}alt="" />
+            <p>
+              {recentPrompt}
+            </p>
+          </div>
+          <div className="result-data">
+            <img src={assets.gemini_icon} alt="" />
+            {loading
+            ?<div className="loader">
+              <hr />
+              <hr />
+              <hr />
+
+            </div>
+            :<p dangerouslySetInnerHTML={{__html:resultData}}></p>
+            }
+            
+          </div>
+
+        </div>
+        }
+        
+
+        <div className="main-bottom">
+          <div className="search-box">
+            <input onChange={(e)=>setInput(e.target.value)} value={input} type='text'placeholder='enter a prompt here'/>
+            <div>
+              <img src={assets.gallery_icon} alt="" />
+              <img src={assets.mic_icon} alt="" />
+              <img onClick={()=>onSent()} src={assets.send_icon} alt="" />
+            </div>
+          </div>
+          <p className="bottom-info">
+            Gemini may display inaccurate info, including about people ,so double check its responses. your private apps
+          </p>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Main
+export default Main;
